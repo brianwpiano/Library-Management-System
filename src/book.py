6ff47ecd0513ae2c89ff8bad98f56e59b23db2fb
brianwpiano <brianwongpiano@gmail.com>
2024-08-24 16:36:05 -0400

    Upload files

---
+++ src/book.py
@@ -0,0 +1,8 @@
+class Book:
+    def __init__(self, title, author, isbn):
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        self.checked_out = False
+
+    # Implement additional book-related methods here
---
+++ src/library.py
@@ -0,0 +1,12 @@
+from book import Book
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    # Implement library management methods here
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    # Add more library management methods as needed
